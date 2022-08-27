b1dda2cd4937b1e524c953866c7b4b96483c8edf
fitabigail <abidinu35@gmail.com>
2022-08-27 08:51:12 +0000

    Add javascript to video on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,44 @@
         menu.classList.toggle('change');
 
     })
+
+    // Section About - Video
+
+    const video = document.querySelector('.video')
+    const btn = document.querySelector('.buttons i')
+    const bar = document.querySelector('.video_bar')
+
+    const playPause =()=>{
+        if(video.paused)    
+        {
+            video.play()
+            btn.className ='far fa-pause-circle'
+            video.style.opacity='.9'
+        }
+        else{
+            video.pause()
+            btn.className ='far fa-play-circle'
+            video.style.opacity='.6'
+        }
+       
+    }
+
+    btn.addEventListener('click', ()=> {
+        playPause()
+    }) 
+
+    //video bar 
+
+    video.addEventListener('timeupdate', ()=>{
+       const barWidth =video.currentTime/video.duration
+       bar.style.width =`${barWidth * 100}%`
+       if(video.ended){
+        btn.className ='far fa-play-circle'
+        video.style.opacity ='0.6'
+       }
+
+
+
+
+    })
+
