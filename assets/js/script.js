b86077512039fb35a4eb79939fe3140fa626096a
fitabigail <abidinu35@gmail.com>
2022-09-03 17:34:22 +0000

    Add javascript code to motgage calculator

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,12 +32,12 @@
     }
 
     btn.addEventListener('click', ()=> {
-        playPause()
+        playPause();
     }) 
 
     //video bar 
 
-    video.addEventListener('timeupdate', ()=>{
+    video.addEventListener('timeupdate', () =>{
        const barWidth =video.currentTime/video.duration
        bar.style.width =`${barWidth * 100}%`
        if(video.ended){
@@ -46,9 +46,62 @@
        }
 
 
-
-
     })
 
 /* MORTGAGE CALCULATOR */
 
+const mortgageAmountInput = document.querySelector(".mortgage_amount");
+const mortgageYearsInput = document.querySelector(".mortgage_years");
+const interestRateInput = document.querySelector(".interest_rate");
+
+const mortgageEMIValue = document.querySelector(".mortgage_emi .value");
+const totalInterestValue = document.querySelector(".total_interest .value");
+const totalAmountValue = document.querySelector(".total_amount .value");
+
+const calculateBtn = document.querySelector(".calc_btn");
+const resetBtn = document.querySelector("reset-btn");
+
+let mortgageAmount = parseFloat(mortgageAmountInput.value);
+let mortgageYears = parseFloat(mortgageYearsInput.value);
+let interestRate = parseFloat(interestRateInput.value);
+
+
+let interest = interestRate / 12 / 100;
+
+const calculateEMI = () => {
+    let emi = mortgageAmount * interest * (Math.pow(1 + interest, mortgageYears) / (Math.pow(1 + interest, mortgageYears) - 1));
+    return(emi);
+};
+
+
+const updateData = (emi) =>{
+    mortgageEMIValue.innerHTML = Math.round(emi);
+
+    let totalAmount = Math.round(mortgageYears * emi); 
+    totalAmountValue.innerHTML = totalAmount;
+
+    let totalInterestPayable = Math.round(totalAmount - mortgageAmount);
+    totalInterestValue.innerHTML = totalInterestPayable;
+}
+
+const refreshInputValues = () =>{
+    mortgageAmount = parseFloat(mortgageAmountInput.value);
+    mortgageYears = parseFloat(mortgageYearsInput.value);
+    interestRate = parseFloat(interestRateInput.value);
+
+
+    interest = interestRate / 12 / 100;
+};
+
+const init = () => {
+    refreshInputValues();
+   let emi = calculateEMI();
+   updateData(emi);
+};
+
+init();
+
+
+
+
+calculateBtn.addEventListener("click", init);
