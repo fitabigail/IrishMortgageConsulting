54c91df50d894a99b47217e02094ccf81e859138
fitabigail <abidinu35@gmail.com>
2022-09-06 10:48:06 +0000

    Fixed some errror from lighthouse google dev tolls

--- assets/js/javasript2.js
+++ assets/js/javasript2.js
@@ -2,43 +2,34 @@
 
 const menu = document.querySelector(".menu");
 const navbar = document.querySelector(".navbar");
-
-menu.addEventListener('click', ()=>{
-
-    navbar.classList.toggle('change');
-    menu.classList.toggle('change');
-
+menu.addEventListener('click', () => {
+  navbar.classList.toggle('change');
+  menu.classList.toggle('change');
 });
 
 // Chart mortgage loan
 
 let myChart;
-
-
- const displayChart = (totalInterestPayableValue) => {
+const displayChart = (totalInterestPayableValue) => {
   const ctx = document.getElementById("myChart").getContext("2d");
   myChart = new Chart(ctx, {
     type: "pie",
-    data: { 
+    data: {
       labels: ["Total Interest", "Mortgage Amount"],
-      datasets: [
-        {
-          data: [totalInterestPayableValue, mortgageAmount],
-          backgroundColor: ['#b3420e','#312f2c'],
-          borderWidth: 0,
-        },
-      ],
+      datasets: [{
+        data: [totalInterestPayableValue, mortgageAmount],
+        backgroundColor: ['#b3420e', '#312f2c'],
+        borderWidth: 0,
+      }, ],
     },
   });
 };
- 
+
 const updateChart = (totalInterestPayableValue) => {
   myChart.data.datasets[0].data[0] = totalInterestPayableValue;
-  myChart.data.datasets[0].data[1] =  mortgageAmount;
+  myChart.data.datasets[0].data[1] = mortgageAmount;
   myChart.update();
 };
-
-
 
 // Mortgage Calculator 
 
@@ -52,88 +43,61 @@ const mortgageEMIValue = document.querySelector(".mortgage_emi .value");
 const totalInterestValue = document.querySelector(".total_interest .value");
 const totalAmountValue = document.querySelector(".total_amount .value");
 
-
-
 const calculateBtn = document.querySelector(".calc_btn");
-
 
 let mortgageAmount = mortgageAmountInput.value;
 let mortgageYears = mortgageYearsInput.value * 12;
 let interestRate = interestRateInput.value;
 
-
 let interest = interestRate / 12 / 100;
 
-const calculateEMI = () => {   
-   
+const calculateEMI = () => {
+  let emi = mortgageAmount * interest * (Math.pow(1 + interest, mortgageYears) / (Math.pow(1 + interest, mortgageYears) - 1));
+  return (emi);
+};
+const updateData = (emi) => {
+  mortgageEMIValue.innerHTML = Math.round(emi);
 
-    let emi = mortgageAmount * interest * (Math.pow(1 + interest, mortgageYears) / (Math.pow(1 + interest, mortgageYears) - 1));
-    return(emi);
+  let totalAmount = Math.round(mortgageYears * emi);
+  totalAmountValue.innerHTML = totalAmount;
+
+  let totalInterestPayable = Math.round(totalAmount - mortgageAmount);
+  totalInterestValue.innerHTML = totalInterestPayable;
+
+  if (myChart) {
+    updateChart(totalInterestPayable);
+  } else {
+    displayChart(totalInterestPayable);
+  }
 };
 
-
-const updateData = (emi) =>{
-    mortgageEMIValue.innerHTML = Math.round(emi);
-
-    let totalAmount = Math.round(mortgageYears * emi); 
-    totalAmountValue.innerHTML = totalAmount;
-
-    let totalInterestPayable = Math.round(totalAmount - mortgageAmount);
-    totalInterestValue.innerHTML = totalInterestPayable;
-
-    if (myChart) {
-      updateChart(totalInterestPayable);
-    } else {
-      displayChart(totalInterestPayable);
-    }
-};
-
-const refreshInputValues = () =>{
-    mortgageAmount = mortgageAmountInput.value;
-    mortgageYears = mortgageYearsInput.value * 12;
-    interestRate = interestRateInput.value;
-
-
-    interest = interestRate / 12 / 100;
+const refreshInputValues = () => {
+  mortgageAmount = mortgageAmountInput.value;
+  mortgageYears = mortgageYearsInput.value * 12;
+  interestRate = interestRateInput.value;
+  interest = interestRate / 12 / 100;
 };
 
 const init = () => {
-    refreshInputValues();
-   let emi = calculateEMI();
-   updateData(emi);
+  refreshInputValues();
+  let emi = calculateEMI();
+  updateData(emi);
 };
-
 init();
-
 calculateBtn.addEventListener("click", init);
-
-
-
-
-
-
-
 
 // Booking Form
 
-document.querySelector('.booking_btn').addEventListener
-('click', () => {
- document.querySelector('.booking').style.display = 'none';
- document.querySelector('.form_wp').style.cssText = 'opacity: 1; visibility: visible';
-   });
-
-document.querySelector('.close_btn').addEventListener('click', () =>{
-   document.querySelector('.booking').style.display = 'flex';
-   document.querySelector('.form_wp').style.cssText = 'opacity: 0; visibility: hidden';
-
+document.querySelector('.booking_btn').addEventListener('click', () => {
+  document.querySelector('.booking').style.display = 'none';
+  document.querySelector('.form_wp').style.cssText = 'opacity: 1; visibility: visible';
+});
+document.querySelector('.close_btn').addEventListener('click', () => {
+  document.querySelector('.booking').style.display = 'flex';
+  document.querySelector('.form_wp').style.cssText = 'opacity: 0; visibility: hidden';
 });
 
 function myBooking() {
-   alert("Thank you for your booking request. Soon one of our team will be in touch with you.");
-   window.location.href='https://8000-fitabigail-irishmortgag-tkpn56x4ty1.ws-eu63.gitpod.io/mortgage-calculator.html?';
+  alert("Thank you for your booking request. Soon one of our team will be in touch with you.");
+  window.location.href = 'https://8000-fitabigail-irishmortgag-tkpn56x4ty1.ws-eu63.gitpod.io/mortgage-calculator.html?';
 }
-
-
-
-
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,80 +2,50 @@
 
      const menu = document.querySelector(".menu");
      const navbar = document.querySelector(".navbar");
- 
-     menu.addEventListener('click', ()=>{
- 
+     menu.addEventListener('click', () => {
          navbar.classList.toggle('change');
          menu.classList.toggle('change');
- 
      });
- 
+
      // Section About - Video
- 
+
      const video = document.querySelector('.video');
      const btn = document.querySelector('.buttons i');
      const bar = document.querySelector('.video_bar');
- 
-     const playPause =()=>{
-         if(video.paused)    
-         {
+     const playPause = () => {
+         if (video.paused) {
              video.play();
-             btn.className ='far fa-pause-circle';
-             video.style.opacity='.9';
-         }
-         else{
+             btn.className = 'far fa-pause-circle';
+             video.style.opacity = '.9';
+         } else {
              video.pause();
-             btn.className ='far fa-play-circle';
-             video.style.opacity='.6';
+             btn.className = 'far fa-play-circle';
+             video.style.opacity = '.6';
          }
-        
      };
- 
-     btn.addEventListener('click', ()=> {
+
+     btn.addEventListener('click', () => {
          playPause();
-     }); 
- 
-     //video bar 
- 
-     video.addEventListener('timeupdate', () =>{
-        const barWidth =video.currentTime/video.duration;
-        bar.style.width =`${barWidth * 100}%`;
-        if(video.ended){
-         btn.className ='far fa-play-circle';
-         video.style.opacity ='0.6';
-        }
- 
- 
      });
 
-//Scroll Button
+     //video bar 
 
-document.querySelector('.scroll_btn').addEventListener(
-    'click', () =>{
-        document.querySelector('html').style.scrollBehavior = 'smooth';
-        setTimeout(() =>{
-            document.querySelector('html').style.scrollBehavior = 'unset';
-        }), 1000;
-    }
-);
+     video.addEventListener('timeupdate', () => {
+         const barWidth = video.currentTime / video.duration;
+         bar.style.width = `${barWidth * 100}%`;
+         if (video.ended) {
+             btn.className = 'far fa-play-circle';
+             video.style.opacity = '0.6';
+         }
+     });
 
+     //Scroll Button
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     document.querySelector('.scroll_btn').addEventListener(
+         'click', () => {
+             document.querySelector('html').style.scrollBehavior = 'smooth';
+             setTimeout(() => {
+                 document.querySelector('html').style.scrollBehavior = 'unset';
+             }), 1000;
+         }
+     );
