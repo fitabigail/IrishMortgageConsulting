5c73df41fb5cff286a1d7e0ec3c382752861ee05
fitabigail <abidinu35@gmail.com>
2022-09-06 18:02:42 +0000

    Added javascript to contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,22 @@
              }), 1000;
          }
      );
+
+     // CONTACT US FORM
+
+     function sendEmail(){
+        Email.send({
+            Host : "smtp.yahoo.com",
+            Username : "abi_dinu@yahoo.com",
+            Password : "BunditA76",
+            To : 'abidinu35@gmail.com',
+            From : document.getElementById('email').value,
+            Subject : "New Contact Form Enquiry",
+            Body : "Name: " + document.getElementById('name').value
+            + "<br> Email:" + document.getElementById('email').value
+            + "<br> Phone no:" + document.getElementById('phone').value
+            + "<br> Message:" + document.getElementById('message').value
+        }).then(
+          message => alert("Message send succefuly")
+        );
+     }
